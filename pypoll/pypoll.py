d678c8e0a1fb3c90281fcc54fa2adcf832d0a2d5
cglovertenn <cglovertenn@gmail.com>
2019-08-24 17:36:56 -0500

    COMPLETE

--- pypoll/pypoll.py
+++ pypoll/pypoll.py
@@ -14,6 +14,7 @@ def datafetch(csv):
     corrvote = 0
     livote = 0
     toolvote = 0
+    winner = 0
     for row in csv:
         votes = row[2]
         total += 1
@@ -25,13 +26,21 @@ def datafetch(csv):
             livote = livote + 1
         if "O'Tooley" in votes:
             toolvote = toolvote + 1
+        if khanvote > corrvote > livote > toolvote:
+            winner = "Khan"
+        if corrvote > khanvote > livote > toolvote:
+            winner = "Correy"
+        if livote > khanvote > corrvote > toolvote:
+            winner = "Li"
+        if toolvote > khanvote > corrvote > livote:
+            winner = "O'Tooley"
     
     khan_pct = round((khanvote/total)*100, 2)
     corr_pct = round((corrvote/total)*100, 2)
     li_pct = round((livote/total)*100, 2)
     tool_pct = round((toolvote/total)*100, 2)
     total = khanvote + corrvote + livote + toolvote
-    return [total, khanvote, corrvote, livote, toolvote, khan_pct, corr_pct, li_pct, tool_pct]
+    return [total, khanvote, corrvote, livote, toolvote, khan_pct, corr_pct, li_pct, tool_pct, winner]
 
 # Set source file path
 election_data_csv = os.path.join("election_data.csv")
@@ -55,7 +64,7 @@ Correy: {analysis[6]}% ({analysis[2]})
 Li: {analysis[7]}% ({analysis[3]})
 O'Tooley: {analysis[8]}% ({analysis[4]})
 -------------------------
-Winner: Khan
+Winner: {analysis[9]}
 -------------------------
 """)
 
